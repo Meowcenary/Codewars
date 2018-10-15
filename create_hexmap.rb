5c42fb4c409f5baa759fd5e2603d3bfd904c69d9
Eric Newman <newmaneric5@gmail.com>
2018-10-14 23:39:38 -0500

    working out some basics with a hex grid

--- create_hexmap.rb
+++ create_hexmap.rb
@@ -1,3 +1,69 @@
+class Hex
+  attr_accessor :content
+  attr_reader :column, :row
+
+  def initialize(column, row)
+    @column = column
+    @row = row
+    @content = nil
+  end
+
+  def coordindate
+    "|#{@column}, #{@row}|"
+  end
+end
+
+class HexGrid
+  def initialize(height, length)
+    @hexes = (0..height).map { |row| (0..length).map { |column| Hex.new(column, row)} }
+  end
+
+  def find_by_coordinate(row, column)
+    @hexes[row][column]
+  end
+
+  def print
+    grid = ''
+    offset = false
+
+    @hexes.each do |row|
+      if offset
+        grid += '   '
+        offset = false
+      else
+        offset = true
+      end
+
+      row.each do |hex|
+        grid += hex.coordindate
+      end
+
+      # grid_connectors = '   \\  /'*@hexes.first.length
+      ##{grid_connectors}
+      grid +=  "\n\n"
+    end
+
+    puts grid
+  end
+
+  def adjacent(hex)
+    adjacent_hexes = []
+
+    # top right
+    adjacent_hexes << @hexes[hex.row - 1][hex.column + 1]
+    # top left
+    adjacent_hexes << @hexes[hex.row - 1][hex.column]
+    # right
+    adjacent_hexes << @hexes[hex.row][hex.column + 1]
+    # left
+    adjacent_hexes << @hexes[hex.row][hex.column - 1]
+    # bottom right
+    adjacent_hexes << @hexes[hex.row + 1][hex.column + 1]
+    # bottom left
+    adjacent_hexes << @hexes[hex.row + 1][hex.column]
+  end
+end
+
 def print_hex_grid(hex_grid)
   offset = false
 
@@ -18,6 +84,21 @@ def print_hex_grid(hex_grid)
 end
 
 def create_hex_grid(height, length)
-  (0..height).map { |h| (0..length).map { |l| "#{h}, #{l}" } }
+  (0..height).map { |h| (0..length).map { |l| "#{l}, #{h}" } }
 end
 
+def adjacent_hexes(row, column, hex_grid)
+  adjacent_hexes = []
+  # top right
+  adjacent_hexes << hex_grid[row - 1][column + 1]
+  # top left
+  adjacent_hexes << hex_grid[row - 1][column]
+  # right
+  adjacent_hexes << hex_grid[row][column + 1]
+  # left
+  adjacent_hexes << hex_grid[row][column - 1]
+  # bottom right
+  adjacent_hexes << hex_grid[row + 1][column + 1]
+  # bottom left
+  adjacent_hexes << hex_grid[row + 1][column]
+end
