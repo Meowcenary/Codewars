a58aa0b0878938c6b55fbfe2dd3336d3924c9696
Eric Newman <newmaneric5@gmail.com>
2018-10-19 10:46:30 -0500

    Decided to implement game logic from 'Blue & Grey' SPI games version. Hopefully can seperate hex and grid logic from game logic easily.

--- Hexmap/game.rb
+++ Hexmap/game.rb
@@ -23,19 +23,22 @@ class Game
 
     strength_ratio = 6 if strength_ratio > 6
 
-    combat_roll = rand(0..5)
+    combat_roll = rand(0..9)
 
     puts "Strength Ratio: #{strength_ratio}\nCombat Roll: #{combat_roll + 1}"
+
+    # calculate column shift from strength ratio
+
     # strength ratio determines column, roll determines damage
-    # this will need reiteration, but just trying to get a basic
-    # system working
+    # combat result table (crt) taken from SPI version of Blue & Grey
+    #                      atk/def  1-5   1-4   1-3   1-2   1-1   2-1   3-1   4-1   5-1   6-1
     combat_result_table = {
-                            1 => [0, 0, 0, 1, 1, 2],
-                            2 => [0, 0, 1, 2, 2, 2],
-                            3 => [1, 2, 2, 3, 4, 5],
-                            4 => [2, 3, 3, 3, 4, 5],
-                            5 => [2, 3, 3, 4, 5, 6],
-                            6 => [2, 3, 5, 5, 6, 6]
+                            1  => ['ar', 'ar', 'dr', 'dr', 'dr', 'dr', 'de', 'de', 'de', 'de'],
+                            2  => ['ar', 'ar', 'ar', 'dr', 'dr', 'dr', 'dr', 'dr', 'de', 'de'],
+                            3  => ['ar', 'ar', 'ar', 'ar', 'dr', 'dr', 'dr', 'dr', 'dr', 'de'],
+                            4  => ['ae', 'ar', 'ar', 'ar', 'ar', 'dr', 'dr', 'dr', 'dr', 'dr'],
+                            5  => ['ae', 'ae', 'ar', 'ar', 'ar', 'ar', 'dr', 'ex', 'ex', 'dr'],
+                            6  => ['ae', 'ae', 'ae', 'ae', 'ar', 'ar', 'ex', 'ex', 'ex', 'ex']
                           }
 
     combat_result_table[strength_ratio][combat_roll]
--- Hexmap/test.rb
+++ Hexmap/test.rb
@@ -1,12 +1,13 @@
 Dir["./*.rb"].each {|file| require file }
 
+def create_units(units)
+  units.map do |unit_type|
+    Unit.new(unit_type.first, unit_type.last)
+  end
+end
+
 units = [['artillery', 2], ['cavalry', 1], ['infantry', 3]]
 
-@red_units = units.map do |unit_type|
-  Unit.new(unit_type.first, unit_type.last)
-end
-
-@blue_units = units.map do |unit_type|
-  Unit.new(unit_type.first, unit_type.last)
-end
-
+@red_units = create_units(units)
+@blue_units = create_units(units)
+@game = Game.new
