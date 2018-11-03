043c84f601967bb2f8fb4b5a015c5b1ca48a3dde
Eric Newman <newmaneric5@gmail.com>
2018-11-02 22:41:51 -0500

    adding comments to better understand the base code

--- Hexmap/hexgridwidget-master/hexgridwidget.js
+++ Hexmap/hexgridwidget-master/hexgridwidget.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-    /*global $, document*/
+    // global $, document
     $.fn.hexGridWidget = function (radius, columns, rows, cssClass) {
+        // ???
         'use strict';
+
+        // called throughout, will actually create the polygon element in the DOM
         var createSVG = function (tag) {
             return $(document.createElementNS('http://www.w3.org/2000/svg', tag || 'svg'));
         };
+
         return $(this).each(function () {
             var element = $(this),
+                    // the encapsulated event that is triggered when a hex is clicked
                     hexClick = function () {
                         var hex = $(this);
                         element.trigger($.Event('hexclick', hex.data()));
@@ -20,6 +25,7 @@ $(document).ready(function() {
                     toPoint = function (dx, dy) {
                         return Math.round(dx + center.x) + ',' + Math.round(dy + center.y);
                     };
+
             for (row = 0; row < rows; row++) {
                 for (column = 0; column < columns; column++) {
                     center = {x:Math.round((1 + 1.5 * column) * radius), y: Math.round(height * (1 + row * 2 + (column % 2)))};
@@ -35,6 +41,8 @@ $(document).ready(function() {
                         'class':cssClass,
                         tabindex:1
                     })
+                    // .on adds trigger for click that calls hexClick function to raise expected event
+                    // .attr gives the appended polygon element the data attributes 'hex-row' and 'hex-column'
                     .appendTo(svgParent).data({center:center, row:row, column:column}).on('click', hexClick).attr({'hex-row': row, 'hex-column': column});
                 }
             }
